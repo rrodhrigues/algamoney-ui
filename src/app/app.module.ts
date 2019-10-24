8b78e9d7b108ee675761127e72b66dd6f0aa4826
Renato <rrodrigys@gmail.com>
2019-10-23 22:01:18 -0300

    feat: Add 10.13 add query for people

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { AppComponent } from './app.component';
 import { from } from 'rxjs';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LancamentosPesquisaComponent,
-    NavbarComponent
+    NavbarComponent,
+    PessoasPesquisaComponent
   ],
   imports: [
     BrowserModule,
